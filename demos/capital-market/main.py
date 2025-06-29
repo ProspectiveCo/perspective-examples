802baba04cef5bc0c775a42850f53eb63e7419a7
xdatanomad <parham.parvizi@gmail.com>
2025-06-29 15:42:34 -0400

    grooming main.py

--- demos/capital-market/main.py
+++ demos/capital-market/main.py
@@ -6,18 +6,22 @@ import pro_capital_markets.blotter as blotter
 
 
 def main():
-    print("Pro Capital Markets")
+    print("\n*** Pro Capital Markets ***\n")
     
     print("Symbols: ", constants.UNIQUE_SYMBOLS)
 
+    print("\nFetching historical data...\n")
     # market.fetch_one_symbol("AAPL")
     # market.fetch_all_symbols()
     historical_df = pd.read_parquet(constants.MARKET_FILE)
-    print("Historical DataFrame:\n", historical_df.head())
+    print("Historical DataFrame:\n", historical_df.head(n=10))
 
+    print("\nGenerating blotter...\n")
     blotter.run_generate_blotter()
     blotter_df = pd.read_parquet(constants.BLOTTER_FILE)
-    print("Blotter DataFrame:\n", blotter_df.head())
+    print("\nBlotter DataFrame:\n", blotter_df.head(n=10))
+
+    print("\nConverting blotter to CSV...\n")
     blotter_df.to_csv(constants.BLOTTER_FILE.with_suffix('.csv'), index=False)
     
     # dumping events to CSV
